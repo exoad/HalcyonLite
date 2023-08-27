8f152dc43b52d433e1ab81b3fa3921b1a70d272d
Jack Meng <jackmeng0814@gmail.com>
2023-08-27 02:31:16 -0400

    `add`

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 /// This is the main entrypoint into the Halcyon program.
 void main() {
@@ -10,51 +11,13 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
         body: Center(
-          child: PlayState(),
-        ),
+            child: Column(children: [
+          ChangeNotifierProvider(create: (_) {}, child: const Text("yes")),
+        ])),
       ),
     );
-  }
-}
-
-class PlayState extends StatefulWidget {
-  const PlayState({
-    super.key,
-  });
-
-  @override
-  State<PlayState> createState() => _PlayStateState();
-}
-
-class _PlayStateState extends State<PlayState> {
-  bool isPlaying = false;
-
-  @override
-  void initState() {
-    super.initState();
-    _init_audio();
-  }
-
-  void _init_audio() async {}
-
-  void _alter() async {
-    setState(() => isPlaying = !isPlaying);
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return GestureDetector(
-        onTap: _alter,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(isPlaying ? "Playing media!" : "Playing nothing...", style: const TextStyle(fontWeight: FontWeight.w600, fontSize: 32)),
-            const SizedBox(height: 15),
-            Icon(isPlaying ? Icons.pause_rounded : Icons.play_arrow_rounded, size: 64),
-          ],
-        ));
   }
 }
--- lib/tailwind/tailwind.dart
+++ lib/tailwind/tailwind.dart
@@ -3,13 +3,15 @@ import 'dart:collection';
 import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
 
-final Tailwind tailwind = Tailwind();
+// ignore: library_private_types_in_public_api
+final _Tailwind tailwind = _Tailwind();
 
-class Tailwind {
+/// The **Tailwind** class provides the defaults for all audio interfacing. It represents the minimal barebones interface between [audioplayers] package and the Halcyon program.
+class _Tailwind {
   final AudioPlayer _player1, _player2;
   final ListQueue<Source> _audioQueue;
 
-  Tailwind()
+  _Tailwind()
       : _player1 = AudioPlayer(),
         _player2 = AudioPlayer(),
         _audioQueue = ListQueue<Source>();
@@ -18,10 +20,35 @@ class Tailwind {
   /// This should be used with caution if the AudioPlayer is still in the process of playing
   void _load(AudioPlayer player, Source source) => player.setSource(source);
 
+  /// Loads a source into the queue. **Does not start playing**
   void enqueue(Source source) => _audioQueue.add(source);
+
+  /// Starts playing the audio. This is also a `resume` functionality.
+  void play() {}
+
+  /// Stops playing the audio and does not modify the current timestamp.
+  void pause() {}
+
+  /// Stops playing the audio and resets the current timestamp to `0`.
+  void stop() {}
+
+  /// Play the next song in the queue. Skips the current.
+  ///
+  /// [dropCurr] - Whether the song to skip over should be removed from the queue entirely
+  void next(bool dropCurr) {}
+
+  /// Play the previous song in the queue. Skips the current.
+  ///
+  /// [dropCurr] - Whether the song to skip over should be removed from the queue entirely
+  void back(bool dropCurr) {}
+
+  /// Returns a reference to the current audio queue.
+  ///
+  /// **Use with caution**
+  ListQueue<Source> peek() => _audioQueue;
 }
 
-class TailwindModel extends ChangeNotifier {}
+class TailwindModel extends _Tailwind with ChangeNotifier {}
 
 typedef TAudioFileFormatInfo = ({
   String id,
@@ -29,10 +56,18 @@ typedef TAudioFileFormatInfo = ({
   String formalName,
 });
 
-final TSupportedFormats = List.unmodifiable(<TAudioFileFormatInfo>[
+final supportedFormats = List.unmodifiable(<TAudioFileFormatInfo>[
   (id: "MP3", endings: ["mp3"], formalName: "MPEG Audio Layer III"),
   (id: "WAV", endings: ["wav"], formalName: "WaveForm"),
-  (id: "OGG", endings: ["ogg", "opus", "oga"], formalName: "OGG"),
+  (id: "OGG", endings: ["ogg", "oga"], formalName: "OGG"),
   (id: "FLAC", endings: ["flac"], formalName: "Free Lossless Audio Codec"),
   (id: "AAC", endings: ["aac"], formalName: "Advanced Audio Coding")
 ]);
+
+List<String> supportedEndings() {
+  List<String> res = [];
+  for (var e in supportedFormats) {
+    e.endings.forEach(res.add);
+  }
+  return res;
+}
